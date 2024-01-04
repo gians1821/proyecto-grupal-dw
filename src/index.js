f94b33468ef6e45a7cff48b27a1bb433e972a550
gians1821 <gian.samana.1801@gmail.com>
2024-01-04 17:40:29 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const contentSidebar = document.getElementById("contentSidebar");
+const sidebarDerecha = document.getElementById("sidebarDerecha");
+const sidebarIzquierda = document.getElementById("sidebarIzquierda");
+
+sidebarDerecha.addEventListener("click", function (e) {
+  contentSidebar.scrollTo({
+    left: (contentSidebar.scrollLeft += 490),
+    behavior: "cubic-bezier(0.1, 0.7, 1.0, 0.1)",
+  });
+});
+
+
+sidebarIzquierda.addEventListener("click", function (e) {
+  contentSidebar.scrollTo({
+    left: (contentSidebar.scrollLeft -= 490),
+    behavior: "cubic-bezier(0.1, 0.7, 1.0, 0.1)",
+  });
+});
+
+console.log(contentSidebar);
+
+const flagsElement = document.getElementById('flags');
+const textsToChange = document.querySelectorAll('[data-section]');
+
+const changeLenguaje = async (lenguaje) => {
+    const requestJson = await fetch(`/lenguajes/${lenguaje}.json`);
+    const texts = await requestJson.json();
+
+    for (const textToChange of textsToChange) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        if (section == "s7" && (value == "titulo"|| value == "texto")) {
+            textToChange.innerHTML = texts[section][value]
+        } else {
+            if (section == "s7" && value != "boton") {
+                textToChange.placeholder = texts[section][value]
+            } else {
+                textToChange.value = texts[section][value]
+            }
+        }
+
+        if (section != "s7") {
+            textToChange.innerHTML = texts[section][value];
+        }
+    }
+};
+
+flagsElement.addEventListener("click", (e) => {
+    changeLenguaje(e.target.parentElement.dataset.lenguaje);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'principal': '#4c4c4c',
+        'secundario': '#7db6e9',
+        'blanco': '#ffffff',
+        'extra': '#191919',
+      },
+      width: {
+        '90': '21rem',
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
+export default m
